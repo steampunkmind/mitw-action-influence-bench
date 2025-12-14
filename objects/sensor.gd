66ae2f67eec3984c747a1db55d1c34e76fdb6f69
Steampunk Mind <asteampunkmind@gmail.com>
2025-12-14 17:10:18 -0500

    Adds get formula type.

--- objects/sensor.gd
+++ objects/sensor.gd
@@ -53,35 +53,36 @@ func get_dict() -> Dictionary:
 	return _dict
 	
 	
+func get_formula_type(key: String) -> SensorFormula:
+	var formula_type_name = key.get_basename()
+	var result = _types.get(formula_type_name)
+	if !result:
+		print(formula_type_name + " formula type not found.")
+	return result
+
+
 ### Formulas ###
 func set_formula(formula: Formula) -> void:
 	var expressions = formula.get_expressions()
 	for key: String in expressions:
 		var expression = expressions.get(key)
-		var formula_type_name = key.get_basename()
-		var formula_type = _types.get(formula_type_name)
-		if formula_type == null:
-			print(formula_type_name + " formula type not found.")
-		else:
+		var formula_type = get_formula_type(key)
+		if formula_type:
 			formula_type.init_expression(expression)
-		
-		get_formulas().set(key, expressions.get(key))
+			_formulas.set(key, expression)
 
 
 func get_formula_value(sensor: Sensor, formulas: Dictionary) -> float:
 	var result = sensor.get_value()
 	SensorFormula.limit_value = false
 	for key: String in formulas.keys():
-		var formula_type_name = key.get_basename() 
-		var formula_type = _types.get(formula_type_name)
-		if formula_type == null:
-			print(formula_type_name + " formula type not found.")
-		else:
+		var formula_type = get_formula_type(key)
+		if formula_type:
 			result = formula_type.get_value(result, key, formulas)
 			formula_type.increment_frame(key, formulas)
 			if formula_type.is_complete():
 				formula_type.set_complete(false)
-				sensor.get_formulas().erase(formula_type_name)
+				sensor._formulas.erase(key)
 		
 	if (SensorFormula.limit_value && result > sensor.get_value()):
 		return sensor.get_value()
@@ -94,12 +95,9 @@ func get_formula_text(sensor_formulas: Dictionary) -> String:
 	# should call to_string method in future SensorFormula Class
 	if (sensor_formulas):
 		for key: String in sensor_formulas:
-			var formula_type_name = key.get_basename()
-			var formula_type = _types.get(formula_type_name)
-			if formula_type == null:
-				print(formula_type_name + " formula type not found.")
-			else:
+			var formula_type = get_formula_type(key)
+			if formula_type:
 				result = formula_type.get_text(result, key, sensor_formulas)
-			result += "\r"
+				result += "\r"
 			
 	return result
