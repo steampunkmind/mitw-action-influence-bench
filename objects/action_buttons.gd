2531601d425195689894dc261c3aa7443053e7a3
Steampunk Mind <asteampunkmind@gmail.com>
2026-05-19 14:48:38 -0400

    Adds spacer before and after action buttons.

--- objects/action_buttons.gd
+++ objects/action_buttons.gd
@@ -39,19 +39,18 @@ func add_action_buttons():
 	new_button_location = $ActionButtonTemplate.position.x
 	for action: Action in MITW.aim_model().get_actions():
 		add_action_button(action)
+		
+	move_child($BottomSpacer, -1)
 	
 	
 func add_action_button(action: Action) -> void:
 	var button = $ActionButtonTemplate.duplicate(1)
-	#var button_margin = button.position.x
 	button.name = action.get_name()
 	button.text = action.get_name()
-	#button.offset_left = new_button_location
 	button.pressed.connect(_action_button_pressed.bind(action))
 	add_child(button)
 	action_buttons.append(button)
 	button.visible = true
-	#new_button_location = button.position.x + (button.size.x * button.get_scale().x) + button_margin
 	
 	
 func show_hide_buttons():
