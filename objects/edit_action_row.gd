b4a9d2b16e254de64ff7a8f7849cac16735def6c
Steampunk Mind <asteampunkmind@gmail.com>
2026-05-20 13:10:16 -0400

    Adds delete action button.

--- objects/edit_action_row.gd
+++ objects/edit_action_row.gd
@@ -1,8 +1,12 @@
 class_name EditActionRow
 extends ColorRect
 
+var _action: Action
+
+signal delete_action_button_pressed
 
 func set_action(action: Action) -> void:
+	_action = action
 	set_name(action.get_name()) # sets name of node
 	set_action_name(action.get_name())
 
@@ -15,3 +19,7 @@ func set_row_location(y: float) -> void:
 	var p = get_position()
 	p.y = y
 	set_position(p)
+
+
+func _on_delete_action_button_pressed() -> void:
+	delete_action_button_pressed.emit()
--- objects/edit_actions.gd
+++ objects/edit_actions.gd
@@ -3,6 +3,7 @@ extends Control
 
 signal model_changed
 
+
 @export var edit_action_row_template: PackedScene
 
 var actions:
@@ -22,7 +23,7 @@ func set_actions(value: Array[Action]):
 
 func clear_edit_action_rows() -> void:
 	for edit_action_row: EditActionRow in _edit_action_rows:
-		remove_child(edit_action_row)
+		$ActionScroll/Actions.remove_child(edit_action_row)
 	_edit_action_rows.clear()
 
 
@@ -30,6 +31,7 @@ func add_edit_action_rows() -> void:
 	for action: Action in actions:
 		var row = edit_action_row_template.instantiate()
 		row.set_action(action)
+		row.delete_action_button_pressed.connect(_delete_action_button_pressed.bind(action))
 		$ActionScroll/Actions.add_child(row)
 		_edit_action_rows.append(row)
 
@@ -37,6 +39,15 @@ func add_edit_action_rows() -> void:
 func _on_add_button_button_up() -> void:
 	var action = Action.new("Action " + str(actions.size() + 1), true)
 	actions.append(action)
+	_model_changed()
+
+
+func _delete_action_button_pressed(action: Action) -> void:
+	actions.erase(action)
+	_model_changed()
+
+
+func _model_changed() -> void:
 	clear_edit_action_rows()
 	add_edit_action_rows()
 	model_changed.emit()
