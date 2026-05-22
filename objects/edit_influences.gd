82cc0cca2409dfc5dba0a2a4e5706c2f72784625
Steampunk Mind <asteampunkmind@gmail.com>
2026-05-21 20:54:08 -0400

    Behavior check box changes action value.

--- objects/edit_influences.gd
+++ objects/edit_influences.gd
@@ -6,3 +6,9 @@ var _action: Action
 func set_action(action: Action) -> void:
 	_action = action
 	set_custom_minimum_size($BehaviorCheckBox.get_minimum_size())
+	$BehaviorCheckBox.set_pressed_no_signal(action.get_behavioral())
+
+
+func _on_behavior_check_box_toggled(toggled_on: bool) -> void:
+	# Have to dirty document here.
+	_action.set_behavioral(toggled_on)
