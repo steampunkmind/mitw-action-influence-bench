5002d95028ef7b5c019986d07cf65df16d08322e
Steampunk Mind <asteampunkmind@gmail.com>
2026-05-24 13:54:39 -0400

    Adds VBox to edit expression.

--- objects/edit_expression.gd
+++ objects/edit_expression.gd
@@ -1,8 +1,19 @@
 class_name EditExpression extends Control
 
-var _expression_type: Dictionary
+var _expression_type: String
+var _expressions
 
 
-func set_expression_type(_expression_type: String) -> void:
-	$ExpressionType.text = _expression_type
-	set_custom_minimum_size($ExpressionType.get_combined_minimum_size())
+func set_expression_type(expression_type: String, expressions) -> void:
+	_expression_type = expression_type
+	_expressions = expressions
+	$VBox/ExpressionType.text = expression_type
+	$VBox/Expressions.text = str(expressions)
+	_set_minimum_y()
+
+
+func _set_minimum_y() -> void:
+	var p: Vector2 = get_combined_minimum_size()
+	p.y += $VBox/ExpressionType.get_combined_minimum_size().y
+	p.y += $VBox/Expressions.get_combined_minimum_size().y
+	set_custom_minimum_size(p)
