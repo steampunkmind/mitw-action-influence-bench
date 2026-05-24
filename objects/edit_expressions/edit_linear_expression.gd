16d592e0fc3ccf74c1ee8b30d9b7bc9dce3c158e
Steampunk Mind <asteampunkmind@gmail.com>
2026-05-24 17:22:56 -0400

    Enters expression value in percent field.

--- objects/edit_expressions/edit_linear_expression.gd
+++ objects/edit_expressions/edit_linear_expression.gd
@@ -3,4 +3,8 @@ class_name EditLinearExpression extends EditExpression
 
 func init(expression_type: String, expression) -> void:
 	super.init(expression_type, expression)
-	
+	$Percent.text = str(expression)
+
+
+func _on_text_edit_text_changed() -> void:
+	print("_on_text_edit_text_changed")
