887c3d1eee9a971da3e62758f2941363f8e690b6
Steampunk Mind <asteampunkmind@gmail.com>
2026-05-23 21:56:03 -0400

    Adds add influence button.

--- objects/edit_influences.gd
+++ objects/edit_influences.gd
@@ -1,7 +1,11 @@
-extends Control
+class_name EditInfluences extends VBoxContainer
 
 var _action: Action
 
 
 func set_action(action: Action) -> void:
 	_action = action
+
+
+func _on_add_influence_button_pressed() -> void:
+	print("_on_add_influence_button_pressed")
