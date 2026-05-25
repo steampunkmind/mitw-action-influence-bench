cad5dfba2c5ef50fecf47e0aa96e4d1e772fb514
Steampunk Mind <asteampunkmind@gmail.com>
2026-05-25 11:23:05 -0400

    Changes linear expression interface from percent to value.

--- objects/edit_expressions/edit_linear_expression.gd
+++ objects/edit_expressions/edit_linear_expression.gd
@@ -3,7 +3,7 @@ class_name EditLinearExpression extends EditExpression
 
 func init(expression_type: String, expression) -> void:
 	super.init(expression_type, expression)
-	$Percent.text = str(expression)
+	$Value.text = str(expression)
 
 
 func _on_text_edit_text_changed() -> void:
