8d238fd94bce1803b11d7f02c19d01baa85dd9d7
Steampunk Mind <asteampunkmind@gmail.com>
2026-05-26 21:17:47 -0400

    Adds edit dict entry.

--- objects/edit_expressions/edit_inflow_percent_expression.gd
+++ objects/edit_expressions/edit_inflow_percent_expression.gd
@@ -1,9 +1,20 @@
 class_name EditInflowPercentExpression extends EditExpression
 
+@export var edit_dict_entry_template: PackedScene
+
 
 func init(key: String, expressions) -> void:
 	super.init(key, expressions)
-	$Value.text = str(expressions.get(key))
+	var expression = expressions.get(key)
+	var x: float = $Name.size.x + 12
+	for dict: Dictionary in expression:
+		var edit_dict_entry = edit_dict_entry_template.instantiate()
+		add_child(edit_dict_entry)
+		
+		var p = edit_dict_entry.position
+		p.x = x
+		edit_dict_entry.set_position(p)
+		x += edit_dict_entry.size.x + 20
 
 
 func _on_text_edit_text_changed() -> void:
