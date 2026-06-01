5aa16dc5b7aa90bfc51ae75d124f0035c78898ae
Steampunk Mind <asteampunkmind@gmail.com>
2026-06-01 14:15:40 -0400

    Adds spacer before edit expressions.

--- objects/edit_influence.gd
+++ objects/edit_influence.gd
@@ -14,15 +14,15 @@ signal delete_influence
 
 func set_influence(influence: Influence) -> void:
 	_influence = influence
-	$HBox/SensorName.text = influence.get_sensor_name()
-	$HBox/AddEditExpression.get_popup().index_pressed.connect(_on_add_edit_expression_index_pressed)
+	$SensorBox/SensorName.text = influence.get_sensor_name()
+	$SensorBox/AddEditExpression.get_popup().index_pressed.connect(_on_add_edit_expression_index_pressed)
 	for expression_name: String in MITW.get_sensor_formula_types().keys():
-		$HBox/AddEditExpression.get_popup().add_item(expression_name)
+		$SensorBox/AddEditExpression.get_popup().add_item(expression_name)
 		
 	var expressions: Dictionary = influence.get_formula().get_expressions()
 	for key: String in expressions.keys():
 		var expression: EditExpression = _expression_by_type(key, expressions)
-		$VBox.add_child(expression)
+		$ExpressionBox/Expressions.add_child(expression)
 		_edit_expressions.append(expression)
 
 
@@ -49,11 +49,11 @@ func _expression_by_type(type: String, expressions: Dictionary) -> EditExpressio
 
 
 func _on_add_edit_expression_index_pressed(index) -> void:
-	var expression_name = $HBox/AddEditExpression.get_popup().get_item_text(index)
+	var expression_name = $SensorBox/AddEditExpression.get_popup().get_item_text(index)
 	var expressions: Dictionary = _influence.get_formula().get_expressions()
 	expressions.set(expression_name, "-") # get default expression by type
 	var expression: EditExpression = _expression_by_type(expression_name, {})
-	$VBox.add_child(expression)
+	$ExpressionBox/Expressions.add_child(expression)
 	_edit_expressions.append(expression)
 	_model_changed()
 
